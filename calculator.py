dace968bb0cf6e867ef590764ab93722e987b38d
CADisHARD <mandy1299901474@gmail.com>
2023-05-26 13:51:53 -0400

    calculator update

--- calculator.py
+++ calculator.py
@@ -1,48 +1,45 @@
 from tkinter import *
-# Try to push
-# Function to perform calculation
-def calculate():
-    num1 = float(entry1.get())
-    num2 = float(entry2.get())
-    operator = entry3.get()
 
-    result = None
-    if operator == '+':
-        result = num1 + num2
-    elif operator == '-':
-        result = num1 - num2
-    elif operator == '*':
-        result = num1 * num2
-    elif operator == '/':
-        if num2 != 0:
-            result = num1 / num2
-        else:
-            result = "Error: Division by zero"
-    
-    # Update the result label
-    label_result.config(text="Result: " + str(result))
+def button_click(number):
+    current = entry.get()
+    entry.delete(0, END)
+    entry.insert(END, current + str(number))
+
+def button_clear():
+    entry.delete(0, END)
+
+def button_equal():
+    expression = entry.get()
+    result = eval(expression)
+    entry.delete(0, END)
+    entry.insert(END, result)
 
 # Create the Tkinter window
 window = Tk()
 window.title("Calculator")
 
-# Create entry fields for number input
-entry1 = Entry(window, width=10)
-entry1.pack(side=LEFT)
+# Create the entry field for display
+entry = Entry(window, width=25, borderwidth=5)
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
-entry2 = Entry(window, width=10)
-entry2.pack(side=LEFT)
+# Create the number buttons
+for i in range(10):
+    button = Button(window, text=str(i), padx=20, pady=10, command=lambda i=i: button_click(i))
+    button.grid(row=(i+3)//3, column=(i-1)%3, padx=5, pady=5)
 
-entry3 = Entry(window, width=5)
-entry3.pack(side=LEFT)
+# Create the operator buttons
+operators = ['+', '-', '*', '/']
+for i, operator in enumerate(operators):
+    button = Button(window, text=operator, padx=20, pady=10, command=lambda operator=operator: button_click(operator))
+    button.grid(row=i+3, column=3, padx=5, pady=5)
 
-# Create button for calculation
-button_calculate = Button(window, text="Calculate", command=calculate)
-button_calculate.pack(side=LEFT)
+# Create the clear button
+button_clear = Button(window, text="C", padx=20, pady=10, command=button_clear)
+button_clear.grid(row=2, column=0, padx=5, pady=5)
 
-# Create label for displaying result
-label_result = Label(window, text="Result: ")
-label_result.pack(side=LEFT)
+# Create the equal button
+button_equal = Button(window, text="=", padx=20, pady=10, command=button_equal)
+button_equal.grid(row=2, column=1, columnspan=2, padx=5, pady=5)
 
 # Run the Tkinter event loop
 window.mainloop()
